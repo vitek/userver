2080b22ccf759a65ac4c67f38a761e99f3f1eae5
Victor Makarov <vitja@yandex-team.ru>
2024-08-01 19:23:48 +0300

    fix

--- samples/hello_service/upy.py
+++ samples/hello_service/upy.py
@@ -1,7 +1,12 @@
+import time
+
+
 def init(idx):
     print(f'I am interpreter {idx}')
 
     def script():
+        for _ in range(10000):
+            pass
         return f'Hello from python interpreter {idx}\n'
 
     return script
